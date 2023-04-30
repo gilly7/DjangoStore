df05efb3aa3f37df3b59726234beba0bd08f4c0d
ngeno.gilbert <ngeno.gilbert@nlske.com>
2023-04-30 10:51:15 +0300

    djangoadmin config

--- djangostore/store/admin.py
+++ djangostore/store/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
+
+
+from .models import Category, Product
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+
+   prepopulated_fields = {'slug' : ('name',)}
+   
+   
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+
+   prepopulated_fields = {'slug' : ('title',)}   
--- djangostore/store/models.py
+++ djangostore/store/models.py
@@ -24,7 +24,7 @@ class Product(models.Model):
     
     price = models.DecimalField(max_digits=4, decimal_places=2)
     
-    #image = models.ImageField(upload_to='images/') 
+    image = models.ImageField(upload_to='images/') 
     
     class Meta:
         verbose_name_plural='products'
