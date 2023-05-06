275c60a540fa38b61308e87bc8db095ccb28fce3
ngeno.gilbert <ngenogilbert07@gmail.com>
2023-05-06 23:23:26 +0300

    create django app-cart, configure basic urls and views for the cart

---
+++ djangostore/cart/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def cart_summary(request):
+    
+    pass
+
+def cart_add(request):
+    
+    pass
+
+def cart_delete(request):
+    
+    pass
+
+def cart_update(request):
+    
+    pass
+
+
