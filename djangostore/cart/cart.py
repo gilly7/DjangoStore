56e5b18c514c653da2c7d20b12277e24d8282dcc
ngeno.gilbert <ngenogilbert07@gmail.com>
2023-05-07 23:49:29 +0300

    AJAX integration, add functionality

--- djangostore/cart/cart.py
+++ djangostore/cart/cart.py
@@ -13,6 +13,21 @@ class Cart():
             cart = self.session['session_key']= {}
             
         self.cart = cart    
+        
+    def add(self, product, product_qty):   
+        
+        product_id = str(product_id)
+        
+        if product_id in self.cart:
+            
+            self.cart[product_id]['qty'] = product_qty
+            
+        else:
+            
+            self.cart[product_id] = {'price': str(product.price),'qty':product_qty}
+            
+        self.session.modified = True    
+                
 
 
 
